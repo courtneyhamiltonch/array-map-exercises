20807cb090aa04ee77d8be705437b737cf3c3945
Courtney Hamilton <courtney.hamilton.CH@gmail.com>
2023-11-14 13:16:07 -0600

    final

---
+++ index.js
@@ -0,0 +1,106 @@
+// Double the numbers in array
+/*const arr = [2, 5, 100]
+const result = arr.map(function(num){
+    return num * 2
+})
+console.log(result)
+
+// Turn array to string
+const arr = [2, 5, 100]
+const result = arr.map(function(num){
+    return num.toString()
+})
+console.log(result)
+
+// Capitalize the first letter of each name and make the rest of the characters lowercase
+const names = ["john", "JACOB", "jinGleHeimer", "schmidt"]
+const result = names.map(function(letters){
+    return letters.charAt(0, 4, 9, 21).toUpperCase() + letters.slice(1, 12).toLowerCase()
+})
+console.log(result)
+
+ // Return string of names only 
+ var items = [
+      {
+          name: "Angelina Jolie",
+          age: 80
+      },
+      {
+          name: "Eric Jones",
+          age: 2
+      },
+      {
+          name: "Paris Hilton",
+          age: 5
+      },
+      {
+          name: "Kayne West",
+          age: 16
+      },
+      {
+          name: "Bob Ziroll",
+          age: 100
+      }
+  ];
+  var names = items.map(function(item) {
+    return item['name'];
+  });
+  console.log(names)
+var items = [
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+]
+let age = 21
+ const result = items.map(function(person){
+    if (person.age >= 21) {
+        return(person.name + " can go to The Matrix")
+      } else {
+         return(person.name + " is under age!!")
+      }
+ })
+ console.log(result)
+
+ var items = [
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+]
+const result = items.map(function(obj){
+    return "<h1>" +obj.name +"</h1>" + "<h2>" + obj.age +"</h2>"
+})
+console.log(result)*/
